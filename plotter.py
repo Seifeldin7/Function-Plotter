485a501e6b42bcadf757cee2fa08c470df592f13
Seif Eldin Mohamed <s.seif26@yahoo.com>
2021-08-28 18:16:39 +0200

    add validation

--- plotter.py
+++ plotter.py
@@ -7,22 +7,17 @@ import matplotlib.pyplot as plt
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 
 import numpy as np
-
+import re
 
 class Canvas(FigureCanvas):
     def __init__(self, parent):
         self.fig, self.ax = plt.subplots(figsize=(5, 4), dpi=200)
         super().__init__(self.fig)
-        self.x = np.linspace(-2, 2, 100)
+        self.x = np.linspace(-100, 100, 100)
         self.y = None
+        self.max_val = 0
+        self.min_val = 0
         self.setParent(parent)
-
-        """ 
-        Matplotlib Script
-        """
-        self.x = np.linspace(-2, 2, 100)
-        y = self.x
-        self.ax.plot(self.x, y)
 
         self.ax.set(xlabel='x', ylabel='y',
                     title='Function')
@@ -34,8 +29,15 @@ class Canvas(FigureCanvas):
     def setY(self, y):
         self.y = eval(y)
 
+    def setMin(self, min_val):
+        self.min_val = min_val
+
+    def setMax(self, max_val):
+        self.max_val = max_val
+
     def plot(self):
         self.ax.cla()
+        self.x = np.linspace(self.min_val, self.max_val, 100)
         self.ax.plot(self.x, self.y)
         self.ax.grid()
         self.draw()
@@ -44,10 +46,6 @@ class App(QMainWindow):
     def __init__(self):
         super(App, self).__init__()
         self.title = "Function Plotter"
-        self.height = 800
-        self.width = 1000
-        self.top = 0
-        self.left = 0
         self.chart = Canvas(self)
 
         self.layout = QVBoxLayout()
@@ -55,28 +53,83 @@ class App(QMainWindow):
 
     def init_ui(self):
         self.setWindowTitle(self.title)
-        self.setGeometry(self.left, self.top, self.width, self.height)
         self.setLayout(self.layout)
         win = QWidget()
         self.layout.addWidget(win)
         grid_layout = QGridLayout()
+        h_layout = QHBoxLayout()
         win.setLayout(grid_layout)
         plot_field = QLineEdit(self)
-        plot_field.setStyleSheet("background-color: black; color: white;")
+        min_field = QLineEdit(self)
+        max_field = QLineEdit(self)
+        min_lbl = QLabel("Min: ", self)
+        max_lbl = QLabel("Max: ", self)
         plot_btn = QPushButton("Plot", self)
-        plot_btn.setStyleSheet("background-color: #51E000; color: white; ")
-        plot_btn.clicked.connect(lambda: self.plot(plot_field))
+
+        min_field.setStyleSheet("background-color: black; color: white;")
+        min_field.setFixedWidth(150)
+        min_lbl.setStyleSheet("color: white;")
+        max_field.setStyleSheet("background-color: black; color: white;")
+        max_field.setFixedWidth(150)
+        max_lbl.setStyleSheet("color: white;")
+        plot_field.setStyleSheet("background-color: black; color: white;")
+        plot_btn.setStyleSheet("background-color: rgb(23, 82, 198); color: white; ")
+
+        plot_btn.clicked.connect(lambda: self.plot(plot_field, max_field, min_field))
 
         grid_layout.addWidget(plot_field, 1, 1)
         grid_layout.addWidget(plot_btn, 1, 2)
-        grid_layout.addWidget(self.chart, 2, 1)
+        min_max_widget = QWidget(self)
+        min_max_widget.setLayout(h_layout)
+        h_layout.addWidget(min_lbl)
+        h_layout.addWidget(min_field)
+        h_layout.addWidget(max_lbl)
+        h_layout.addWidget(max_field)
+        grid_layout.addWidget(min_max_widget, 2, 1)
+        grid_layout.addWidget(self.chart, 3, 1)
         win.show()
 
-    def plot(self, plot_field):
+    def plot(self, plot_field, max_field, min_field):
+        max_value = max_field.text()
+        min_value = min_field.text()
         y = plot_field.text()
-        y = y.replace('x', 'self.x')
+        message = self.validate_input(y, min_value, max_value)
+        if message:
+            self.display_dialog(message)
+            return
+        max_value = float(max_value)
+        min_value = float(min_value)
+        y = y.replace('x', 'self.x').replace('^', '**')
         self.chart.setY(y)
+        self.chart.setMax(max_value)
+        self.chart.setMin(min_value)
         self.chart.plot()
+
+    def validate_input(self, input_val, min_val, max_val):
+        message = None
+        if not min_val:
+            message = "Min value should not be empty."
+        elif not max_val:
+            message = "Max value should not be empty."
+        elif not input_val:
+            message = "Function should not be empty."
+        elif 'x' not in input_val:
+            message = "Input should be a function of x."
+        elif not self.validate_by_regex(input_val):
+            message = "Please check the input format."
+        return message
+
+    def display_dialog(self, message):
+        mbox = QMessageBox()
+        mbox.setText(message)
+        mbox.setStyleSheet("color: black;")
+        mbox.setStandardButtons(QMessageBox.Ok)
+        mbox.exec_()
+
+    def validate_by_regex(self, input_val):
+        regex = re.compile(r'((x|\d+)(\+|-|\*|/|\**)?)+', re.I)
+        match = regex.fullmatch(str(input_val).replace(' ', ''))
+        return bool(match)
 
 def main():
     palette = QPalette()
